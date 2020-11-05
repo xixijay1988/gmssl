97d5dd29702548d05c5750baf068fcfdc0dc2014
Dawn Xi <xixijay1988@gmail.com>
2020-11-05 11:54:49 +0800

    add sm2 generate key method

--- gmssl/sm2.py
+++ gmssl/sm2.py
@@ -1,16 +1,19 @@
 import binascii
 from random import choice
 from . import sm3, func
+import secrets
+
 # 选择素域，设置椭圆曲线参数
 
 default_ecc_table = {
     'n': 'FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123',
     'p': 'FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF',
-    'g': '32c4ae2c1f1981195f9904466a39c9948fe30bbff2660be1715a4589334c74c7'\
+    'g': '32c4ae2c1f1981195f9904466a39c9948fe30bbff2660be1715a4589334c74c7' \
          'bc3736a2f4f6779c59bdcee36b692153d0a9877cc62a474002df32e52139f0a0',
     'a': 'FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC',
     'b': '28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93',
 }
+
 
 class CryptSM2(object):
 
@@ -22,7 +25,18 @@ class CryptSM2(object):
         self.public_key = public_key
         self.para_len = len(ecc_table['n'])
         self.ecc_a3 = (
-            int(ecc_table['a'], base=16) + 3) % int(ecc_table['p'], base=16)
+                              int(ecc_table['a'], base=16) + 3) % int(ecc_table['p'], base=16)
+        self.ecc_table = ecc_table
+        assert mode in (0, 1), 'mode must be one of (0, 1)'
+        self.mode = mode
+
+    def __init__(self, ecc_table=default_ecc_table, mode=0):
+        """
+        mode: 0-C1C2C3, 1-C1C3C2 (default is 1)
+        """
+        self.para_len = len(ecc_table['n'])
+        self.ecc_a3 = (
+                              int(ecc_table['a'], base=16) + 3) % int(ecc_table['p'], base=16)
         self.ecc_table = ecc_table
         assert mode in (0, 1), 'mode must be one of (0, 1)'
         self.mode = mode
@@ -50,7 +64,7 @@ class CryptSM2(object):
     def _double_point(self, Point):  # 倍点
         l = len(Point)
         len_2 = 2 * self.para_len
-        if l< self.para_len * 2:
+        if l < self.para_len * 2:
             return None
         else:
             x1 = int(Point[0:self.para_len], 16)
@@ -128,7 +142,7 @@ class CryptSM2(object):
             form = form * 3
             return form % (X3, Y3, Z3)
 
-    def _convert_jacb_to_nor(self, Point): # Jacobian加重射影坐标转换成仿射坐标
+    def _convert_jacb_to_nor(self, Point):  # Jacobian加重射影坐标转换成仿射坐标
         len_2 = 2 * self.para_len
         x = int(Point[0:self.para_len], 16)
         y = int(Point[self.para_len:len_2], 16)
@@ -146,10 +160,15 @@ class CryptSM2(object):
         else:
             return None
 
+    def generate(self):
+        privKey = secrets.token_hex(32)
+        pubKey = self._kg(int(privKey, 16), self.ecc_table['g'])
+        return privKey, pubKey
+
     def verify(self, Sign, data):
         # 验签函数，sign签名r||s，E消息hash，public_key公钥
         r = int(Sign[0:self.para_len], 16)
-        s = int(Sign[self.para_len:2*self.para_len], 16)
+        s = int(Sign[self.para_len:2 * self.para_len], 16)
         e = int(data.hex(), 16)
         t = (r + s) % int(self.ecc_table['n'], base=16)
         if t == 0:
@@ -171,7 +190,7 @@ class CryptSM2(object):
         return (r == ((e + x) % int(self.ecc_table['n'], base=16)))
 
     def sign(self, data, K):  # 签名函数, data消息的hash，private_key私钥，K随机数，均为16进制字符串
-        E = data.hex() # 消息转化为16进制字符串
+        E = data.hex()  # 消息转化为16进制字符串
         e = int(E, 16)
 
         d = int(self.private_key, 16)
@@ -183,35 +202,35 @@ class CryptSM2(object):
         R = ((e + x) % int(self.ecc_table['n'], base=16))
         if R == 0 or R + k == int(self.ecc_table['n'], base=16):
             return None
-        d_1 = pow(d+1, int(self.ecc_table['n'], base=16) - 2, int(self.ecc_table['n'], base=16))
-        S = (d_1*(k + R) - R) % int(self.ecc_table['n'], base=16)
+        d_1 = pow(d + 1, int(self.ecc_table['n'], base=16) - 2, int(self.ecc_table['n'], base=16))
+        S = (d_1 * (k + R) - R) % int(self.ecc_table['n'], base=16)
         if S == 0:
             return None
         else:
-            return '%064x%064x' % (R,S)
+            return '%064x%064x' % (R, S)
 
     def encrypt(self, data):
         # 加密函数，data消息(bytes)
-        msg = data.hex() # 消息转化为16进制字符串
+        msg = data.hex()  # 消息转化为16进制字符串
         k = func.random_hex(self.para_len)
-        C1 = self._kg(int(k,16),self.ecc_table['g'])
-        xy = self._kg(int(k,16),self.public_key)
+        C1 = self._kg(int(k, 16), self.ecc_table['g'])
+        xy = self._kg(int(k, 16), self.public_key)
         x2 = xy[0:self.para_len]
-        y2 = xy[self.para_len:2*self.para_len]
+        y2 = xy[self.para_len:2 * self.para_len]
         ml = len(msg)
-        t = sm3.sm3_kdf(xy.encode('utf8'), ml/2)
-        if int(t,16)==0:
+        t = sm3.sm3_kdf(xy.encode('utf8'), ml / 2)
+        if int(t, 16) == 0:
             return None
         else:
             form = '%%0%dx' % ml
             C2 = form % (int(msg, 16) ^ int(t, 16))
             C3 = sm3.sm3_hash([
-                i for i in bytes.fromhex('%s%s%s'% (x2,msg,y2))
+                i for i in bytes.fromhex('%s%s%s' % (x2, msg, y2))
             ])
             if self.mode:
-                return bytes.fromhex('%s%s%s' % (C1,C3,C2))
+                return bytes.fromhex('%s%s%s' % (C1, C3, C2))
             else:
-                return bytes.fromhex('%s%s%s' % (C1,C2,C3))
+                return bytes.fromhex('%s%s%s' % (C1, C2, C3))
 
     def decrypt(self, data):
         # 解密函数，data密文（bytes）
@@ -219,53 +238,52 @@ class CryptSM2(object):
         len_2 = 2 * self.para_len
         len_3 = len_2 + 64
         C1 = data[0:len_2]
-        
+
         if self.mode:
             C3 = data[len_2:len_3]
             C2 = data[len_3:]
         else:
             C2 = data[len_2:-64]
             C3 = data[-64:]
-            
-        xy = self._kg(int(self.private_key,16),C1)
+
+        xy = self._kg(int(self.private_key, 16), C1)
         # print('xy = %s' % xy)
         x2 = xy[0:self.para_len]
         y2 = xy[self.para_len:len_2]
         cl = len(C2)
-        t = sm3.sm3_kdf(xy.encode('utf8'), cl/2)
+        t = sm3.sm3_kdf(xy.encode('utf8'), cl / 2)
         if int(t, 16) == 0:
             return None
         else:
             form = '%%0%dx' % cl
-            M = form % (int(C2,16) ^ int(t,16))
+            M = form % (int(C2, 16) ^ int(t, 16))
             u = sm3.sm3_hash([
-                i for i in bytes.fromhex('%s%s%s'% (x2,M,y2))
+                i for i in bytes.fromhex('%s%s%s' % (x2, M, y2))
             ])
             return bytes.fromhex(M)
 
+    def _sm3_z(self, data):
+        """
+        SM3WITHSM2 签名规则:  SM2.sign(SM3(Z+MSG)，PrivateKey)
+        其中: z = Hash256(Len(ID) + ID + a + b + xG + yG + xA + yA)
+        """
+        # sm3withsm2 的 z 值
+        z = '0080' + '31323334353637383132333435363738' + \
+            self.ecc_table['a'] + self.ecc_table['b'] + self.ecc_table['g'] + \
+            self.public_key
+        z = binascii.a2b_hex(z)
+        Za = sm3.sm3_hash(func.bytes_to_list(z))
+        M_ = (Za + data.hex()).encode('utf-8')
+        e = sm3.sm3_hash(func.bytes_to_list(binascii.a2b_hex(M_)))
+        return e
 
-     def _sm3_z(self, data):
-         """
-         SM3WITHSM2 签名规则:  SM2.sign(SM3(Z+MSG)，PrivateKey)
-         其中: z = Hash256(Len(ID) + ID + a + b + xG + yG + xA + yA)
-         """
-         # sm3withsm2 的 z 值
-         z = '0080'+'31323334353637383132333435363738' + \
-             self.ecc_table['a'] + self.ecc_table['b'] + self.ecc_table['g'] + \
-             self.public_key
-         z = binascii.a2b_hex(z)
-         Za = sm3.sm3_hash(func.bytes_to_list(z))
-         M_ = (Za + data.hex()).encode('utf-8')
-         e = sm3.sm3_hash(func.bytes_to_list(binascii.a2b_hex(M_)))
-         return e
+    def sign_with_sm3(self, data, random_hex_str=None):
+        sign_data = binascii.a2b_hex(self._sm3_z(data).encode('utf-8'))
+        if random_hex_str is None:
+            random_hex_str = func.random_hex(self.para_len)
+        sign = self.sign(sign_data, random_hex_str)  # 16进制
+        return sign
 
-     def sign_with_sm3(self, data, random_hex_str=None):
-         sign_data = binascii.a2b_hex(self._sm3_z(data).encode('utf-8'))
-         if random_hex_str is None:
-             random_hex_str = func.random_hex(self.para_len)
-         sign = self.sign(sign_data, random_hex_str) #  16进制
-         return sign
-
-     def verify_with_sm3(self, sign, data):
-         sign_data = binascii.a2b_hex(self._sm3_z(data).encode('utf-8'))
-         return self.verify(sign, sign_data)
+    def verify_with_sm3(self, sign, data):
+        sign_data = binascii.a2b_hex(self._sm3_z(data).encode('utf-8'))
+        return self.verify(sign, sign_data)
